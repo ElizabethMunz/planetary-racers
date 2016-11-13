9b2e9a6dc8e7bf7e5ea6930594e76cbafab71d7a
Will Johannesen <willjohannesen@gmail.com>
2016-11-13 08:24:39 -0500

    how far I've gotten with gravity

---
+++ Assets/Scripts/gravBubble.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gravBubble : MonoBehaviour {
+
+	public float planetForce = 120f;
+
+
+	void OnTriggerStay2D (Collider other) 
+	{
+		if (other.attachedRigidbody) 
+			other.attachedRigidbody.AddForce (Vector2.down * planetForce, ForceMode.Acceleration);
+	}
+
+}
